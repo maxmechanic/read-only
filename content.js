7ecce0d8370e051ebbbe7bf67d13bd92b8c8bcfe
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2016-09-11 08:19:41 -0700

    Add MutationObserver, break up + compose fns

--- content.js
+++ content.js
@@ -1,6 +1,18 @@
+const inputTypes = ['input', 'textarea']
+
+const collectElements = selector => document.querySelectorAll(selector)
+
 const disableElements = els =>
   Array.from(els)
        .forEach(el => el.disabled = true)
 
-disableElements(document.querySelectorAll('input'))
-disableElements(document.querySelectorAll('textarea'))
+const collectAndDisableElements = type => disableElements(collectElements(type))
+
+const makeReadOnly = fn => inputs => () => inputs.forEach(fn)
+
+const readOnly = makeReadOnly(collectAndDisableElements)(inputTypes)
+
+const observer = new MutationObserver(readOnly)
+observer.observe(document.body, {childList: true, subtree: true})
+
+readOnly()
